8688379f6c1994b6c2b4753ef5e6cd6293d2aaf9
Andrew Marsh <andrewmarsh42@outlook.com>
2018-01-07 20:41:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let yearsArray = [];
+let d = new Date();
+let year = d.getFullYear();
+
+for (i = (year - 70); i <= (year - 20); i++) {
+    yearsArray.push("" + i + "");
+}
+// Loops through birth dates from 70 years from this year to 20 years from this year, then pushes the numbers into the yearsArray array.
+
+$('#selectYear').empty();
+$.each(yearsArray, function (i, p) {
+    $('#selectYear').append($('<option></option>').val(p).html(p));
+});
+// Empties the #selectYear dropdown menu then pushes the values of the array into the dropdown menu as selectable values
+
+$(document).ready(function () {
+    $("#buttonGet").click(function () {
+        let selectedValue = $("#selectYear").val();
+        // This will retrieve the user's selection as an integer and save it as the "selectedValue" variable
+
+        let userAge = 2018 - selectedValue;
+        // Calculates the user's age based on the current year
+
+        function maxHeartRate() {
+            if (userAge >= 18 && userAge <= 82) {
+                let mhr = (Math.round(205.8 - (0.685 * userAge)));
+                $("#divResult").empty();
+                $("#divResult").append("<strong>" + mhr + "</strong>" + ", but potentially as low as " + (mhr - 6) + " and as high as " + (mhr + 6) + ".");
+            } else {
+                return null;
+            }
+        }
+        // Calculates the user's max heart rate, then uses jQuery to append the "divResult" div with the value returned
+
+        maxHeartRate();
+        // Runs the function
+    });
+});
